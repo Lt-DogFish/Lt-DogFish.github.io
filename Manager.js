6be2a7cd56b500eb94b307cb20e8023c60eb5a26
Lt-DogFish <rajishanivish@gmail.com>
2025-03-10 19:22:04 -0500

    1. Changed theme style and added colors, 2. Added Button click evenListen.JS and added CSS animations for button click. 3. Added transition animatons for button hover and click

--- Manager.js
+++ Manager.js
@@ -4,18 +4,27 @@ fetchAndSetInnerHtml("homePage.html");
 const Profile = document.getElementById("profile");
 const Projects = document.getElementById("projects");
 
+//const button = document.getElementsByClassName("button");
+
 
 async function fetchAndSetInnerHtml(path)
 {
 	try
 	{
-		await fetch(path).then(response => response.text()).then(fetchedHtml => Main.innerHTML = fetchedHtml);
+		await fetch(path)
+				.then(response => response.text())
+				.then(fetchedHtml => Main.innerHTML = fetchedHtml);
 	}
 	catch(err)
 	{
 		console.log(err);
 	}
 }
+
+document.querySelectorAll(".button")
+	.forEach(button => {button.addEventListener("click", () => {
+		button.classList.add("clicked"); setTimeout(() => {button.classList.remove("clicked");}, 300);});});
+
 
 //Home.addEventListener("click", () => fetchAndSetInnerHtml("index.html"));
 Home.addEventListener("click", () => fetchAndSetInnerHtml("homePage.html"));
